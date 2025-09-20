4c90e0230d6278d91315bd16d9b89088f2f00c2f
Mateo Urrutia <retrogaming251@gmail.com>
2025-09-20 19:15:49 -0300

    Refactor main application: update logging initialization in main.go to include application name and version, streamline watcher service messages, and improve overall structure for clarity and organization.

--- cmd/quiver/main.go
+++ cmd/quiver/main.go
@@ -1,32 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/rabbytesoftware/quiver/cmd/quiver/ui"
 
+	"github.com/rabbytesoftware/quiver/internal/core/metadata"
 	"github.com/rabbytesoftware/quiver/internal/core/watcher"
 )
 
 func main() {
 	watcher := watcher.NewWatcherService()
 	
-	go func() {
-		time.Sleep(500 * time.Millisecond)
-		
-		for {
-			time.Sleep(5 * time.Second)
-			watcher.Info("Quiver TUI application started")
-			watcher.Debug("Initializing watcher service...")
-			watcher.Info("Watcher service ready")
-			watcher.Warn("This is a sample warning message")
-			watcher.Error("This is a sample error message")
-			watcher.Info("Try commands like /help, /filter error, /level warn, /pause, /resume, /clear")
-		}
-	}()
-	
 	err := ui.RunUI(watcher)
 	if err != nil {
 		watcher.Unforeseen(err.Error())
 	}
+
+	go func() {
+		time.Sleep(5 * time.Second)
+	
+		watcher.Info(fmt.Sprintf(
+		"%s %s - Initializing...",
+			metadata.GetName(), 
+			metadata.GetVersion(),
+		))
+	}()
 }
