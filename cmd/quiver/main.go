fa36d4aeee99e1cd6cecff2864ecc84b414c6df3
Mateo Urrutia <retrogaming251@gmail.com>
2025-09-28 15:55:29 -0300

    Enhance Makefile and main application for multi-platform icon support: Add new targets for generating icons and building macOS .app bundles in the Makefile. Integrate icon management in the main application to utilize embedded icons, improving the user interface experience.

--- cmd/quiver/main.go
+++ cmd/quiver/main.go
@@ -3,23 +3,28 @@ package main
 import (
 	"fmt"
 
+	"github.com/rabbytesoftware/quiver/cmd/quiver/assets"
 	"github.com/rabbytesoftware/quiver/cmd/quiver/ui"
 	"github.com/rabbytesoftware/quiver/internal"
 	"github.com/rabbytesoftware/quiver/internal/core/metadata"
 )
 
 func main() {
+	iconManager := assets.NewIconManager()
+	defer iconManager.Cleanup()
+
 	internal := internal.NewInternal()
 	watcher := internal.GetCore().GetWatcher()
 
 	go internal.Run()
 
 	go watcher.Info(fmt.Sprintf(
-		"%s %s - Initializing...",
+		"%s %s '%s' - Initializing with embedded icon support...",
 		metadata.GetName(),
 		metadata.GetVersion(),
+		metadata.GetVersionCodename(),
 	))
-
+	
 	err := ui.RunUI(watcher)
 	if err != nil {
 		watcher.Unforeseen(err.Error())
